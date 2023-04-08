2a7f3a6f6047bdb17ba97b786ce6f1532733c72d
coldsteelpope <coldsteelpope@gmail.com>
2023-04-08 21:57:20 +0900

    add whiteboard feature

--- src/App.js
+++ src/App.js
@@ -1,73 +1,94 @@
-import './App.css';
+import "./App.css";
 import io from "socket.io-client";
-import { useCallback, useRef, useState } from 'react';
-import { useEffect } from 'react';
+import { useCallback, useRef, useState } from "react";
+import { useEffect } from "react";
+import Whiteboard from "./components/Whiteboard";
+import Draggable from "react-draggable";
 
 const pc_config = {
   iceServers: [
-    {
-      urls: [
-        "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302"
-      ]
-    }
-  ]
+    //{
+    //  urls: [
+    //    "stun:stun.l.google.com:19302",
+    //    "stun:stun1.l.google.com:19302",
+    //    "stun:stun2.l.google.com:19302",
+    //    "stun:stun3.l.google.com:19302",
+    //    "stun:stun4.l.google.com:19302",
+    //  ],
+    //},
+  ],
 };
-const roomName = "1234"
-//const SOCKET_SERVER_URL = "http://localhost:5000";
-const SOCKET_SERVER_URL = "https://webcam-backend-13oo.onrender.com";
+const roomName = "1234";
+const SOCKET_SERVER_URL = "http://localhost:5000";
+//const SOCKET_SERVER_URL = "https://webcam-backend-13oo.onrender.com";
+
+let myDataChannel = {};
 
 const Video = ({ stream, muted }) => {
-	const ref = useRef(null);
-	const [isMuted, setIsMuted] = useState(false);
+  const ref = useRef(null);
+  const [isMuted, setIsMuted] = useState(false);
 
-	useEffect(() => {
-		if (ref.current) ref.current.srcObject = stream;
-		if (muted) setIsMuted(muted);
-	}, [stream, muted]);
 
-	return (
-		<>
-			<video ref={ref} muted={isMuted} autoPlay />
-		</>
-	);
+  useEffect(() => {
+    if (ref.current) ref.current.srcObject = stream;
+    if (muted) setIsMuted(muted);
+  }, [stream, muted]);
+
+  return (
+    <>
+      <Draggable>
+        <video
+          className="border border-dark shadow p-1 mb-3 bg-body rounded"
+          ref={ref}
+          muted={isMuted}
+          autoPlay
+          style={{
+            maxHeight: "23vh",
+            maxWidth: "23vw",
+            border: "2px solid black",
+            boxShadow: " 0px 0px 10px rgba(0, 0, 0, 0.5)",
+            marginLight: "20px",
+            zIndex: "1",
+          }}
+        />
+      </Draggable>
+    </>
+  );
 };
 
-
 const App = () => {
-	const socketRef = useRef();
-	const pcsRef = useRef({});
-	const localVideoRef = useRef(null);
-	const localStreamRef = useRef();
-	const [users, setUsers] = useState([]);
+  const socketRef = useRef();
+  const pcsRef = useRef({});
+  const localVideoRef = useRef(null);
+  const dataChannel = useRef(); // eslint-disable-line no-unused-vars
+  const localStreamRef = useRef();
+  const [users, setUsers] = useState([]);
+  const [showWhiteboard, setShowWhiteboard] = useState(true); // eslint-disable-line no-unused-vars
+  const [isCameraOn, setIsCameraOn] = useState(true); // eslint-disable-line no-unused-vars
+  const [isMicOn, setIsMicOn] = useState(true); // eslint-disable-line no-unused-vars
+  const [lines, setLines] = useState([]);
+  
 
-  const GetLocalStream = useCallback(async() => {
-    try
-    {
+  const GetLocalStream = useCallback(async () => {
+    try {
       // 로컬 스트림 정보 받아오기
-        const localStream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: true
-        });
-        localStreamRef.current = localStream;
-        if(localVideoRef.current) localVideoRef.current.srcObject = localStream
-        if(!socketRef.current) return;
-        socketRef.current.emit("join_room", {
-          room: roomName
-        });
-    }
-    catch(e)
-    {
+      const localStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      localStreamRef.current = localStream;
+      if (localVideoRef.current) localVideoRef.current.srcObject = localStream;
+      if (!socketRef.current) return;
+      socketRef.current.emit("join_room", {
+        room: roomName,
+      });
+    } catch (e) {
       console.log(`getUserMedia error: ${e}`);
     }
   }, []);
 
   const CreatePeerConnection = useCallback((socketID) => {
-    try
-    {
+    try {
       // Create Peer Connection (관계)
       const pc = new RTCPeerConnection(pc_config);
 
@@ -75,12 +96,9 @@ const App = () => {
       // myPeerConnection.addEventListener("icecandidate", handleIce);
       // 자신의 Ice Candidate를 생성하기 시작합니다.
       pc.onicecandidate = (e) => {
-        if(!(socketRef.current && e.candidate))
-        {
+        if (!(socketRef.current && e.candidate)) {
           return;
-        }
-        else
-        {
+        } else {
           console.log("On ICECandidate");
           socketRef.current.emit("candidate", {
             // sender의 RTCIceCandidate
@@ -88,75 +106,77 @@ const App = () => {
             // Candidate를 보내는(Sender) user의 socket id
             candidateSendID: socketRef.current.id,
             // candidate를 받는 user의 socket id
-            candidateReceiveID: socketID
-          })
+            candidateReceiveID: socketID,
+          });
         }
-      }
+      };
 
       // event 생성 중
       pc.oniceconnectionstatechange = (e) => {
         console.log(e);
-      }
+      };
 
       // event 생성 중
       // On Track 작업 성공 여부 기다림
       pc.ontrack = (e) => {
         console.log("on track success");
         setUsers((oldUsers) =>
-          oldUsers.filter((user) => user.id !== socketID)
-          .concat({
-            id: socketID,
-            stream: e.streams[0]
-          })
-        )
-      }
+          oldUsers
+            .filter((user) => user.id !== socketID)
+            .concat({
+              id: socketID,
+              stream: e.streams[0],
+            })
+        );
+      };
 
       // pc에 local stream ref 정보 추가
-      if(localStreamRef.current)
-      {
-        console.log('localstream add');
+      if (localStreamRef.current) {
+        console.log("localstream add");
         localStreamRef.current.getTracks().forEach((track) => {
-          if(!localStreamRef.current)
-          {
+          if (!localStreamRef.current) {
             return;
-          }
-          else
-          {
+          } else {
             pc.addTrack(track, localStreamRef.current);
           }
-        })
-      }
-      else
-      {
-        console.log('no local stream');
+        });
+      } else {
+        console.log("no local stream");
       }
 
       return pc;
-    }
-    catch(e)
-    {
+    } catch (e) {
       console.error(e);
       return undefined;
     }
   }, []);
+
+  console.log(pcsRef.current)
 
   useEffect(() => {
     socketRef.current = io.connect(SOCKET_SERVER_URL);
     GetLocalStream();
 
     //'similar to welcome'
-    socketRef.current.on('all_users', (allUsers) => {
+    socketRef.current.on("all_users", (allUsers) => {
       // 모든 user들에 대해 다음과 같은 작업을 수행
-      allUsers.forEach(async(user) => {
-        if(!localStreamRef.current) return;
-        
+      allUsers.forEach(async (user) => {
+        if (!localStreamRef.current) return;
         // 2~ 새 Peer Connection 생성하기
         const pc = CreatePeerConnection(user.id);
-        if(!(pc && socketRef.current)) return;
+        if (!(pc && socketRef.current)) return;
         // 새로운 Peer Connection을 pcs(pc 모음)에 관리
         pcsRef.current = { ...pcsRef.current, [user.id]: pc };
-        try
-        {
+        try {
+
+          // Create Channel
+
+          dataChannel.current = pc.createDataChannel("chat");
+          dataChannel.current.addEventListener("message", (event) => {
+            setLines((prevLines) => [...prevLines, JSON.parse(event.data)]);
+          });
+          console.log("made data channel");
+
           // offer를 만드는 중
           const localSdp = await pc.createOffer({
             offerToReceiveAudio: true,
@@ -168,110 +188,187 @@ const App = () => {
           socketRef.current.emit("offer", {
             sdp: localSdp,
             offerSendID: socketRef.current.id,
-            offerReceiveID: user.id
+            offerReceiveID: user.id,
           });
-        }
-        catch(e)
-        {
+        } catch (e) {
           console.error(e);
         }
-      })
+      });
     });
 
     // Offer를 받은 경우 (2번 끝)
-    socketRef.current.on(
-      "getOffer",
-      async (data) => {
-        const { sdp, offerSendID } = data;
-        console.log("Get Offer");
-        if(!localStreamRef.current) return;
-        // 관계 만들기 시도 (3번 실행을 위한 준비)
-        const pc = CreatePeerConnection(offerSendID);
-        if(!(pc && socketRef.current)) return;
-        // 새 관계(pc) 저장하기
-        pcsRef.current = {...pcsRef.current, [offerSendID]: pc};
-        try
-        {
-          // 매개변수 꼭 확인하기!
-          await pc.setRemoteDescription(new RTCSessionDescription(sdp));
-          console.log("Answer Set Remote Description Success");
-          const localSdp = await pc.createAnswer({
-            offerToReceiveVideo: true,
-            offerToReceiveAudio: true
-          });
-          await pc.setLocalDescription(new RTCSessionDescription(localSdp));
-          console.log("answer 만들기")
-          // Answer 보내기
-          socketRef.current.emit('answer', {
-            sdp: localSdp,
-            answerSendID: socketRef.current.id,
-            answerReceiveID: offerSendID
-          });
-        }
-        catch(e)
-        {
-          console.error(e);
-        }
+    socketRef.current.on("getOffer", async (data) => {
+      const { sdp, offerSendID } = data;
+      console.log("Get Offer");
+      if (!localStreamRef.current) return;
+      // 관계 만들기 시도 (3번 실행을 위한 준비)
+      const pc = CreatePeerConnection(offerSendID);
+      if (!(pc && socketRef.current)) return;
+      // 새 관계(pc) 저장하기
+      pcsRef.current = { ...pcsRef.current, [offerSendID]: pc };
+      try {
+
+        // Data Channel 만들기
+        pc.addEventListener("datachannel", (event) => {
+          dataChannel.current = event.channel;
+          dataChannel.current.addEventListener("message", (event) => {
+            setLines((prevLines) => [...prevLines, JSON.parse(event.data)]);
+
+            
+          })
+        });
+        console.log("peer b data channel creates")
+
+        // 매개변수 꼭 확인하기!
+        await pc.setRemoteDescription(new RTCSessionDescription(sdp));
+        console.log("Answer Set Remote Description Success");
+        const localSdp = await pc.createAnswer({
+          offerToReceiveVideo: true,
+          offerToReceiveAudio: true,
+        });
+        await pc.setLocalDescription(new RTCSessionDescription(localSdp));
+        console.log("answer 만들기");
+        // Answer 보내기
+        socketRef.current.emit("answer", {
+          sdp: localSdp,
+          answerSendID: socketRef.current.id,
+          answerReceiveID: offerSendID,
+        });
+      } catch (e) {
+        console.error(e);
       }
-    );
+    });
 
     // Answer 받기
-    socketRef.current.on(
-      "getAnswer",
-      (data) => {
-        const { sdp, answerSendID } = data;
-        console.log('Get Answer');
-        const pc = pcsRef.current[answerSendID];
-        if(!pc) return;
-        // 매개 변수 꼭 확인하기!
-        pc.setRemoteDescription(new RTCSessionDescription(sdp));
-      }
-    );
+    socketRef.current.on("getAnswer", (data) => {
+      const { sdp, answerSendID } = data;
+      console.log("Get Answer");
+      const pc = pcsRef.current[answerSendID];
+
+
+      if (!pc) return;
+      // 매개 변수 꼭 확인하기!
+      pc.setRemoteDescription(new RTCSessionDescription(sdp));
+    });
 
     // ICE Candidate 생성 및 서로 주고 받기
-    socketRef.current.on(
-      "getCandidate",
-      async (data) => {
-        const pc = pcsRef.current[data.candidateSendID];
-        if(!pc) return;
-        await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-        console.log("Candidate Add Success");
-      }
-    );
+    socketRef.current.on("getCandidate", async (data) => {
+      const pc = pcsRef.current[data.candidateSendID];
+      if (!pc) return;
+      await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+      console.log("Candidate Add Success");
+    });
 
     // user exit
     socketRef.current.on("user_exit", (data) => {
-      if(!pcsRef.current[data.id]) return;
+      if (!pcsRef.current[data.id]) return;
       pcsRef.current[data.id].close();
       delete pcsRef.current[data.id];
       setUsers((oldUsers) => oldUsers.filter((user) => user.id !== data.id));
     });
 
     return () => {
-      if(socketRef.current)
-      {
+      if (socketRef.current) {
         socketRef.current.disconnect();
       }
       users.forEach((user) => {
-        if(!pcsRef.current[user.id]) return;
+        if (!pcsRef.current[user.id]) return;
         pcsRef.current[user.id].close();
         delete pcsRef.current[user.id];
-      })
-    }
+      });
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-	return (
-		<div>
-			<video
-				muted
-				ref={localVideoRef}
-				autoPlay
-			/>
-			{users.map((user, index) => (
-				<Video key={index} stream={user.stream} />
-			))}
-		</div>
-	);
+  const onClickTmpBtn = (e) => {
+    console.log("click");
+    console.log(myDataChannel);
+    myDataChannel.send("send");
+  }
+
+  
+
+  return (
+    <div>
+
+      {showWhiteboard && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "white",
+            zIndex: 1,
+          }}
+          className="d-flex justify-content-center align-items-center"
+        >
+
+          {/* Whiteboard component */}
+          <Whiteboard 
+            SOCKET_SERVER_URL={SOCKET_SERVER_URL} 
+            dataChannel={dataChannel}
+            lines={lines}
+            pcsRef={pcsRef}
+            setLines={setLines}
+          />
+        </div>
+      )}
+      <div
+        style={{
+          position: "absolute",
+          right: 0,
+          top: 0,
+          bottom: 0,
+          width: "15%",
+          display: "flex",
+          flexDirection: "column",
+          margin: "20px",
+          marginTop: "auto", // add this
+          zIndex: 1,
+        }}
+      >
+        <div className="modal-dialog">
+          <div
+            className="modal-content"
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <Draggable>
+              <video
+                muted
+                ref={localVideoRef}
+                autoPlay
+                className="border border-dark shadow p-1 mb-3 bg-body rounded"
+                style={{
+                  maxHeight: "23vh",
+                  maxWidth: "23vw",
+                  border: "1px solid black",
+                  boxShadow: " 0px 0px 10px rgba(0, 0, 0, 0.5)",
+                  marginLight: "20px",
+                  zIndex: "1",
+                  marginTop: "20px",
+                }}
+              />
+            </Draggable>
+            {users.map((user, index) => (
+              <Video key={index} stream={user.stream} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed-bottom bg-primary mb-3 d-flex justify-content-center bg-opacity-50">
+        <div className="text-light text-center p-3">
+          <i className="fa-duotone fa-microphone"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default App;
---
+++ src/components/Whiteboard.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+
+function Whiteboard({
+  pcsRef,
+  lines,
+  setLines,
+  dataChannel
+}) {
+  const [drawing, setDrawing] = useState(false);
+
+  const [strokeColor, setStrokeColor] = useState("#000000");
+  const [lineWidth, setLineWidth] = useState(5);
+  //const [lines, setLines] = useState([]);
+  const socketRef = useRef();
+  const canvasRef = useRef();
+  //const SOCKET_SERVER_URL = "https://webcam-backend-13oo.onrender.com";
+  const SOCKET_SERVER_URL = "http://localhost:5000";
+
+  useEffect(() => {
+    socketRef.current = io(SOCKET_SERVER_URL);
+
+    socketRef.current.on("whiteboard-data", (data) => {
+      setLines((prevLines) => [...prevLines, data]);
+    });
+  }, [SOCKET_SERVER_URL]);
+
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+    const newLine = {
+      type: "line",
+      color: strokeColor,
+      size: lineWidth,
+      points: [getMousePosition(event)],
+    };
+    setLines((prevLines) => [...prevLines, newLine]);
+    socketRef.current.emit("whiteboard-data", newLine);
+    dataChannel.current.send(JSON.stringify(newLine));
+
+  };
+
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+    const newLine = { ...lines[lines.length - 1] };
+    newLine.points.push(getMousePosition(event));
+    setLines((prevLines) => [...prevLines.slice(0, -1), newLine]);
+    socketRef.current.emit("whiteboard-data", newLine);
+    dataChannel.current.send(JSON.stringify(newLine));
+  };
+
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
+
+  const handleColorChange = (event) => {
+    setStrokeColor(event.target.value);
+  };
+
+  const handleLineWidthChange = (event) => {
+    setLineWidth(event.target.value);
+  };
+
+  const getMousePosition = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+  };
+
+  function drawLine(context, points, strokeColor, lineWidth) {
+    console.log('hi')
+    context.beginPath();
+    context.moveTo(points[0].x, points[0].y);
+    points.slice(1).forEach((point) => {
+      context.lineTo(point.x, point.y);
+    });
+    context.strokeStyle = strokeColor;
+    context.lineWidth = lineWidth;
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.stroke();
+  }
+  
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const context = canvasRef.current.getContext("2d");
+    lines.forEach((line) => {
+      drawLine(context, line.points, line.color, line.size);
+    });
+  }, [lines]);
+
+  if(Object.keys(pcsRef.current).length !== 0)
+  {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "white",
+          zIndex: -1,
+        }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <canvas
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          width={window.innerWidth}
+          height={window.innerHeight}
+          ref={canvasRef}
+        />
+        <div style={{ position: "absolute", top: 10, left: 10 }}>
+          <label htmlFor="color-picker">Color:</label>
+          <input
+            type="color"
+            id="color-picker"
+            value={strokeColor}
+            onChange={handleColorChange}
+          />
+          <br />
+          <label htmlFor="line-width-picker">Line Width:</label>
+          <input
+            type="range"
+            id="line-width-picker"
+            min="1"
+            max="20"
+            value={lineWidth}
+            onChange={handleLineWidthChange}
+          />
+          <button onClick={(e) => {
+            dataChannel.current.send("hi")
+          }}>
+            asfsadf
+          </button>
+        </div>
+      </div>
+    );
+  }
+  else
+  {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+export default Whiteboard;
